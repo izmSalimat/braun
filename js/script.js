f748f9a63a9b38e52ba5afbdd88cbba8f65817b8
izmSalimat <isalimat@mail.ru>
2019-01-04 20:51:30 +0600

    braun

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var menuBlock = document.getElementById('mobileMenuBlock');
+function openMenu() {
+	menuBlock.style.display = 'block';
+}
+function closeMenu() {
+	menuBlock.style.display = 'none';
+}
